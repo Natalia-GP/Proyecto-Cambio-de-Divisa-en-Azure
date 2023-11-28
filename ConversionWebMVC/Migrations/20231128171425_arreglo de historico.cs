66616ee24e1d38f595e208cb12ec07a8543452d5
Gonzalo Vega Ramos <gonzalo.vega@tajamar365.com>
2023-11-28 18:19:18 +0100

    Arreglo 2

---
+++ ConversionWebMVC/Migrations/20231128171425_arreglo de historico.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConversionWebMVC.Migrations
+{
+    public partial class arreglodehistorico : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
